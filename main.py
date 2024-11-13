09a5f53d92427bd4be68853d6148cb149c9cea61
Anders <zapateria@progamer.no>
2024-11-13 23:52:38 +0100

    fixes

--- main.py
+++ main.py
@@ -1,52 +1,68 @@
 import pygame
+import random
 from pygame.locals import (
     K_LEFT,
     K_RIGHT,
     K_ESCAPE,
     K_SPACE,
+    K_UP,
+    K_DOWN,
     KEYDOWN,
     QUIT,
 )
 
+SPEED       = 0.8
+SIZE        = 25
+FRICTION    = 0.01
+GRAVITY     = 0.01
+BOUNCE      = 2
+
 class Player():
+
     def __init__(self,x,y,r):
-        self.name = "Player"
         self.grounded = False
         self.color = (0,250,0)
         (self.x,self.y,self.r) = (x,y,r)
         (self.dx,self.dy) = (0,0)
 
     def tick(self):
-        if self.dy > 0:
-            self.y += self.dy
+        self.x += self.dx
+        self.y += self.dy
+        if abs(self.dx) < 0.1:
+            self.dx = 0
         if self.grounded:
-            self.x += self.dx
             self.dy = 0
             if self.dx > 0:
-                self.dx -= 0.01
+                self.dx -= FRICTION
             if self.dx < 0:
-                self.dx += 0.01
+                self.dx += FRICTION
         else:
-                self.dy -= 0.01
-                self.y -= self.dy
+                self.dy += GRAVITY
 
     def move(self,x = 0,y = 0):
         self.dx = x
+        self.dy = y
 
     def bounce(self):
+        self.dy = -BOUNCE
+        self.y += self.dy
+
+    def drop(self):
         self.dy = 1
+        self.y += self.dy
 
     def check_grounded(self,platforms):
         for platform in platforms:
-            if self.x+self.r/2 >= platform.x and self.x-self.r/2 <= (platform.x+platform.l):
-                if self.y+self.r > platform.y and self.y+self.r <= platform.y+platform.h:
+            if self.x >= platform.x and self.x <= (platform.x+platform.l): # above platform
+                if abs(round(self.y+self.r) - round(platform.y)) <= 3 * (GRAVITY*100): # on platform
                     self.grounded = True
                     return True
         self.grounded = False
         return False
 
     def draw(self,screen):
-        pygame.draw.circle(screen,self.color,(self.x,self.y), 15)
+        pygame.draw.circle(screen,self.color,(self.x,self.y), self.r, 1)
+
 
 class Platform():
     def __init__(self, x, y, l):
@@ -59,9 +75,9 @@ class Platform():
         self.y += y
         self.rect = pygame.Rect(self.x,self.y,self.l, self.h)
 
-
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
+
 
 class Text():
     def __init__(self):
@@ -84,39 +100,32 @@ def main():
     running = True
     x =  1920/2; y = 1280 / 2
 
-    player = Player(x,y-20,15)
+    player = Player(x,y-100,SIZE)
 
     gtxt = Text()
 
     platforms = []
-    platforms.append(Platform(x-80, y+20, 300))
+#    platforms.append(Platform(x-80, y+15, 300))
 
-    platforms.append(Platform(20, 40, 300))
-    platforms.append(Platform(200, 80, 300))
-    platforms.append(Platform(190, 160, 300))
-    platforms.append(Platform(700, 240, 300))
-    platforms.append(Platform(1200, 320, 300))
-    platforms.append(Platform(400, 480, 300))
-    platforms.append(Platform(500, 580, 300))
-    platforms.append(Platform(1500, 640, 300))
+    for _ in range(20): # create platforms at random placements
+        platforms.append(Platform(random.randint(1,1920-100), random.randint(1,1280-100), random.randint(100,400)))
+
 
     while running:
 
         screen.fill((0,0,0))
-
-        gtxt.set_text("Grounded: "+str(player.check_grounded(platforms))+ " dx: "+str(player.dx)+" dy: "+str(player.dy))
-        gtxt.draw(screen)
  
-
- 
-
         keys = pygame.key.get_pressed()
         if keys[K_LEFT] and player.grounded:
-            player.move(x = -1)
+            player.move(x = -SPEED)
         if keys[K_RIGHT] and player.grounded:
-            player.move(x = 1)
-        if keys[K_SPACE] and player.grounded:
+            player.move(x = SPEED)
+        if (keys[K_SPACE] or keys[K_UP]) and player.grounded:
             player.bounce()
+        if keys[K_DOWN] and player.grounded:
+            player.drop()
+
+        player.check_grounded(platforms)
 
         for event in pygame.event.get():
 
@@ -129,9 +138,12 @@ def main():
 
         player.tick()
 
+        gtxt.set_text(f"dx: {player.dx: .2f} dy: {player.dy: .2f} Speed: {SPEED} Friction: {FRICTION} Gravity: {GRAVITY}")
+        gtxt.draw(screen)
+
         player.draw(screen)
         for platform in platforms:
-#            platform.move(y = 0.11)
+#            platform.move(y = 0.01)
             platform.draw(screen)
 
         pygame.display.flip()
